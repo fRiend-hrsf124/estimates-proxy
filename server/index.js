ecb7ad738949754eca39b29f27d16869aadb3099
mattshin64 <mattshin64@gmail.com>
2019-12-12 14:35:22 -0800

    set up static ip

--- server/index.js
+++ server/index.js
@@ -1,13 +1,3 @@
-// var httpProxy = require('http-proxy');
-
-// var proxy = httpProxy.createProxyServer({
-//   target:
-// });
-
-// http.createServer(function(req, res) {
-//   proxy.web(req, res, { target: 'http://mytarget.com:8080' });
-// });
-
 const express = require('express');
 const port = 3000;
 const app = express();
